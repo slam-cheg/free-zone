dd3dd51a4e046020d17d79b1631809a07000ed45
Lev Shushkov <levashushkov@yandex.ru>
2024-07-20 19:19:34 +0300

    desktop layout

---
+++ scripts/data.js
@@ -0,0 +1,72 @@
+export const categories = {
+    all: {
+        id: "all",
+        entries: "all"
+    },
+	design: {
+		id: "design",
+		entries: ["UX/UI-дизайн", "Анимация и иллюстрация", "Моушн-дизайн и 3D", "Графический дизайн и коммуникации", "Дизайн среды и интерьера"],
+	},
+	marketing: {
+		id: "marketing",
+		entries: [],
+	},
+	gamedev: {
+		id: "gamedev",
+		entries: [],
+	},
+	fashion: {
+		id: "fashion",
+		entries: [],
+	},
+	other: {
+		id: "other",
+		entries: ["IT и менеджмент", "Видео и кино", "Современное искусство", "Программирование и софт"],
+	},
+};
+
+export const cards = {
+	"UX/UI-дизайн": {
+		id: "ux-ui-design",
+		entries: {
+			"UX-дизайн": {
+				img: "",
+				descripttion: "Андрей Гаргуль",
+			},
+			"UX: Поведенческое проектирование": {
+				img: "",
+				descripttion: "Дмитрий Карпов",
+			},
+			"Практики цифровой доступности": {
+				img: "",
+				descripttion: "Илья Ковалев",
+			},
+			"Sketch. app": {
+				img: "",
+				descripttion: "Александр Катин",
+			},
+			"Сетки и проектирование сайтов": {
+				img: "",
+				descripttion: "Александр Гладких и Сергей Гуров",
+			},
+		},
+	},
+	"Анимация и иллюстрация": {
+		"Быстрая анимация иллюстрации в After Effects": {
+			img: "",
+			descripttion: "Константин Новиков",
+		},
+		"Реалистичный концепт-арт": {
+			img: "",
+			descripttion: "Николай Разуев",
+		},
+		"Создание 3D-композиции: Houdini, 3D-Coat, рендер": {
+			img: "",
+			descripttion: "JA ZA",
+		},
+		"Создание 3D-композиции: Houdini, 3D-Coat, рендер": {
+			img: "",
+			descripttion: "JA ZA",
+		},
+	},
+};
